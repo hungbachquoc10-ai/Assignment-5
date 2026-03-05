69d94f0732f189392e12bb736fedc8a2b028d53a
hungbachquoc10-ai <hung.bachquoc10@gmail.com>
2026-03-05 23:25:09 +0700

    Update Exercise 3.py

--- Exercise 3.py
+++ Exercise 3.py
@@ -4,8 +4,9 @@ def interactive_sum():
         text = input("Enter your paragraph(exit to quit): ")
         if text.lower() == 'exit':
             break
-        x = re.findall(r"d+", text)
+        x = re.findall(r"\d+", text)
         total = sum([int(n) for n in x])
         print(f"{total}")
 
 interactive_sum()
+
