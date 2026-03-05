744abf9e0f510fe37dffcb5af00e5b16931e76f8
hungbachquoc10-ai <hung.bachquoc10@gmail.com>
2026-03-05 23:19:19 +0700

    Add files via upload

---
+++ Exercise 3.py
@@ -0,0 +1,10 @@
+import re
+def interactive_sum():
+    while True:
+        text = input("Enter your paragraph(exit to quit): ")
+        if text.lower() == 'exit':
+            break
+        x = re.findall(r"\d+", text)
+        total = sum([int(n) for n in x])
+        print(f"{total}")
+interactive_sum()
